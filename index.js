739231375b1771fec2eb721ecff64efc80f3c123
Jeong Mi Park <parkjeongmi0227@gmail.com>
2022-05-17 01:30:14 -0400

    Use bcrypt to encrypt password and create token for login

--- index.js
+++ index.js
@@ -5,18 +5,19 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { User } = require('./models/User');
 const config = require('./config/key');
-
+const cookieParser = require('cookie-parser');
 // Get application/x-ww-form-urlencoded data
 app.use(bodyParser.urlencoded({ extended: true }));
 // Get application/json data
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 mongoose.connect(config.mogoURI)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
-    res.send('Hello World! Happy new wow');
+    res.send('Hello World!');
 });
 
 app.post('/register', (req, res) => {
@@ -33,6 +34,36 @@ app.post('/register', (req, res) => {
     })
 })
 
+app.post('/login', (req, res) => {
+    // 1. Check email data whether it is stored in database
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message:"Email address not found"
+            })
+        }
+        // 2. If this is the case check password data
+        user.comparePassword(req.body.password, (err, isEqual) => {
+            if (!isEqual) {
+                return res.json({ loginSuccess: false, message: "Password does not match" });
+            }
+            // 3. if password matches then create token for user        
+            user.createToken((err, user) => {
+                if (err) return res.status(400).send(err);
+
+                // Save token (cookie, local storage)
+                res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({ loginSuccess: true, userId: user._id });
+
+            })
+        })
+    })
+    
+    
+
+});
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose'); 
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+// generate salt and encrypt password
+const saltRounds = 10; // 10 digits
 // Mongoose model is a wrapper on the Mongoose schema.
 // Mongoose schema defines the strucutre of the document, default values, validators,etc.
 // Whereas a mongoose model provides an interface to the database for creating, querying,updating,
@@ -30,5 +34,44 @@ const userSchema = mongoose.Schema({
     }
 });
 
+// password encrypt part
+// this will be executed before save function
+userSchema.pre('save', function (next) {
+    var user = this;
+    // if user change password, encrypt password
+    if (user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            // If error occur move to next function 
+            if (err) return next(err)
+            // hash pasword need two arguments unencrypted password and salt
+            bcrypt.hash(user.password, salt, function (err,hash) {
+                if (err) return next(err)
+                user.password = hash
+                 // move to next function
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function (unencryptedPw, callback) {
+    bcrypt.compare(unencryptedPw, this.password, function (err, isEqual) {
+        if (err) return callback(err),
+            callback(null,isEqual)
+    })
+}
+userSchema.methods.createToken = function (callback) {
+    // Create token by utilizing jsonwebtoken package
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+    user.token = token;
+    user.save(function (err, user) {
+        if (err) return callback(err)
+        callback(user)
+    })
+    
+}
 const User = mongoose.model('User', userSchema);
 module.exports = { User };
